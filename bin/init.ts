9e320619fefd856f9c070db8acd6e3379734fafb
Uladzislau Sidliarevich <vlsidlyarevich@gmail.com>
2020-05-16 20:28:38 +0300

    Updated lib versions, added useful config stuff.

--- bin/init.ts
+++ bin/init.ts
@@ -1,7 +1,16 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { InitStack } from '../lib/init-stack';
+import {InitStack} from '../lib/init-stack';
 
 const app = new cdk.App();
-new InitStack(app, 'InitStack');
+new InitStack(app, 'InitStack', {
+    env: getContextParam('aws_env_details'),
+    tags: getContextParam('stack_tags')
+});
+
+function getContextParam(param: string, environment = 'dev') {
+    return app.node.tryGetContext(param)[environment]
+}
+
+
